a5f1a4a2cd95cf4f9ed445187331822401d821d8
Zan <zan@nanoscalesimulations.com>
2023-12-22 17:35:35 +0100

    added testing area

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ print('This is our main')
 
 def Subtract(x,y):
     return x-y
-# Test of Subtract
-print(Subtract(5,2))
 
 def Divide(x,y):
     return x/y
-# Test of Divide
-print(Divide(10,2))
 
 def add(x,y):
     return x+y
 
 def times(x,y):
     return x*y
-print(times(2,4))
+
+if __name__ == "__main__":
+    print('testing mulitplication')
+    print(times(2,4))
+    print('testing addition')
+    print(add(10,5))
+    print('testing subtraction')
+    # Test of Subtract
+    print(Subtract(5,2))
+    print('testing division')
+    # Test of Divide
+    print(Divide(10,2))
