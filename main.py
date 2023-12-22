7fb945d35b4fa41426fe1d130d5d4dbb51d3f43f
Saad <you@example.com>
2023-12-22 17:51:55 +0100

    Added the exponential, cosine and sqrt functions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 print('This is our main')
-
+import numpy as np
 def Subtract(x,y):
     return x-y
 
@@ -12,6 +12,15 @@ def add(x,y):
 def times(x,y):
     return x*y
 
+def exponential(a,b):
+    return a**b
+
+def cos(x): #In degrees
+    return np.cos(x)
+
+def sqrt(x):
+    return np.sqrt(x)
+
 if __name__ == "__main__":
     print('testing mulitplication')
     print(times(2,4))
@@ -23,3 +32,9 @@ if __name__ == "__main__":
     print('testing division')
     # Test of Divide
     print(Divide(10,2))
+    print("Testing exponents")
+    print(exponential(3,2))
+    print("Testing the cosine function")
+    print(cos(45))
+    print("Testing square root")
+    print(sqrt(9))
