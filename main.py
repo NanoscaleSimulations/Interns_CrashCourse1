35628cb948c780a26e1f581a710b3b36c4a1c266
jimmyscottss <ahmeddalhachami@gmail.com>
2023-12-22 18:01:39 +0100

    The Implementation of the Quadratic Formula & Sin/Tan Functions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 print('This is our main')
 
 def Subtract(x,y):
@@ -12,6 +14,29 @@ def add(x,y):
 def times(x,y):
     return x*y
 
+def Quadratic(a,b,c):
+    #The Formula will have the imput of (a*x^2)+(b*x)+c=0
+    #It will find the possible values of x
+    
+    if ((b**2)-(4*a*c)) > 0: #When the discriminant is larger than 0; Two real solutions.
+        return ((-b+math.sqrt((b**2)-(4*a*c)))/(2*a),
+                (-b-math.sqrt((b**2)-(4*a*c)))/(2*a))
+        
+    if ((b**2)-(4*a*c)) == 0: #When the discriminant is equal to 0; One real solution.
+        return((-b+math.sqrt((b**2)-(4*a*c)))/(2*a))
+    
+    if ((b**2)-(4*a*c)) < 0: #When the discriminant is less than 0; No real solutions
+        return(print('The Discriminant is less than 0; No real solutions'))
+
+def Sin(x):
+    #Find the value of the Sine function
+    return(math.sin(x))
+
+def Tan(x):
+    #Find the value of the Tangent function
+    return(math.tan(x))
+
+
 if __name__ == "__main__":
     print('testing mulitplication')
     print(times(2,4))
@@ -23,3 +48,13 @@ if __name__ == "__main__":
     print('testing division')
     # Test of Divide
     print(Divide(10,2))
+    # Test of Quadratic Equation
+    print('Test of Quadratic Equation')
+    print(Quadratic(1,3,2))
+    # Test of Sine Function
+    print('Test of Sine Function')
+    print(Sin(math.pi))
+    # Test of the Tangent Function
+    print('Test of the Tangent Function')
+    print(math.pi)
+    
